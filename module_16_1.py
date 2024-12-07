6a74e48bb02eb1ccb0248dab6ff32c53cdfa84e7
anatoliiprk <anatoliiprk@gmail.com>
2024-12-07 20:46:38 +0700

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -5,16 +5,16 @@ app = FastAPI()
 
 @app.get('/')
 async def welcome() -> str:
-    return ('Главная страница')
+    return 'Главная страница'
 
 @app.get('/user/admin')
 async def welcome_admin() -> str:
-    return ('Вы вошли как администратор')
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
 async def welcome_user(user_id: int) -> str:
-    return (f'Вы вошли как пользователь № {user_id}')
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
 async def user_info(username: str, age: int) -> str:
-    return (f'Информация о пользователе. Имя: {username}, возраст: {age}')
+    return f'Информация о пользователе. Имя: {username}, возраст: {age}'
